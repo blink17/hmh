839ac9f0ab970ad5f6a6acd798bedbc62d6a8039
blink17 <blink17@naver.com>
2016-07-21 00:59:17 +0900

    6일차 마무리~

--- code/win32_handmade.cpp
+++ code/win32_handmade.cpp
@@ -1,5 +1,6 @@
 ﻿#include <windows.h>
 #include <stdint.h>
+#include <Xinput.h>
 
 #define internal static
 #define local_persist static
@@ -30,10 +31,46 @@ struct win32_window_dimension
     int32 Height;
 };
 
-global_variable bool g_running = false;
-global_variable win32_offscreen_buffer g_backbuffer;
+// TODO(jungyoun.la): This is a global for now
+global_variable bool GlobalRunning = false;
+global_variable win32_offscreen_buffer GlobalBackbuffer;
 
-win32_window_dimension Win32GetWindowDimension(HWND _Window)
+//~ Begin XInput - 왜 이렇게 추상계층을 뒀을까?
+// NOTE(jungyoun.la): XInputGetState
+#define X_INPUT_GET_STATE(name) DWORD WINAPI name(DWORD dwUserIndex, XINPUT_STATE* pState)
+typedef X_INPUT_GET_STATE(x_input_get_state);
+X_INPUT_GET_STATE(XInputGetStateStub)
+{
+    return 0;
+}
+global_variable x_input_get_state* XInputGetState_ = XInputGetStateStub;
+#define XInputGetState XInputGetState_
+
+// NOTE(jungyoun.la): XInputSetState
+#define X_INPUT_SET_STATE(name) DWORD WINAPI name(DWORD dwUserIndex, XINPUT_VIBRATION* pVibration)
+typedef X_INPUT_SET_STATE(x_input_set_state);
+X_INPUT_SET_STATE(XInputSetStateStub)
+{
+    return 0;
+}
+global_variable x_input_set_state* XInputSetState_ = XInputSetStateStub;
+#define XInputSetState XInputSetState_
+//~ End XInput
+
+internal void Win32LoadXInput(void)
+{
+    HMODULE xInputLibrary = LoadLibraryA("XInput1_4.dll");
+    if (xInputLibrary)
+    {
+        XInputGetState_ = (x_input_get_state*)GetProcAddress(xInputLibrary, "XInputGetState");
+        if (!XInputGetState_) { XInputGetState_ = XInputGetStateStub; }
+
+        XInputSetState_ = (x_input_set_state*)GetProcAddress(xInputLibrary, "XInputSetState");
+        if (!XInputSetState_) { XInputSetState_ = XInputSetStateStub; }
+    }
+}
+
+internal win32_window_dimension Win32GetWindowDimension(HWND _Window)
 {
     win32_window_dimension result;
 
@@ -82,6 +119,7 @@ internal void Win32ResizeDIBSection(win32_offscreen_buffer* _Buffer, int32 _Widt
 
     _Buffer->Width = _Width;
     _Buffer->Height = _Height;
+
     int32 bytesPerPixel = 4;
 
     _Buffer->Info.bmiHeader.biSize = sizeof(_Buffer->Info.bmiHeader);
@@ -101,9 +139,8 @@ internal void Win32ResizeDIBSection(win32_offscreen_buffer* _Buffer, int32 _Widt
     _Buffer->Pitch = _Buffer->Width * bytesPerPixel;
 }
 
-internal void Win32DisplayBufferInWindow(HDC _DeviceContext, 
-                                         int32 _WindowWidth, int32 _WindowHeight,
-                                         win32_offscreen_buffer* _Buffer)
+internal void Win32DisplayBufferInWindow(win32_offscreen_buffer* _Buffer,
+                                         HDC _DeviceContext, int32 _WindowWidth, int32 _WindowHeight)
 {
     StretchDIBits(_DeviceContext, 
                   /*
@@ -128,13 +165,72 @@ LRESULT CALLBACK MainWindowCallback(_In_ HWND   _HWnd,
         case WM_CLOSE:
         {
             // TODO(jungyoun.la): Handle this with a message to the user?
-            g_running = false;
+            GlobalRunning = false;
         } break;
 
         case WM_DESTROY:
         {
             // TODO(jungyoun.la): Handle this as an error - recreate window?
-            g_running = false;
+            GlobalRunning = false;
+        } break;
+
+        case WM_SYSKEYDOWN:
+        case WM_SYSKEYUP:
+        case WM_KEYDOWN:
+        case WM_KEYUP:
+        {
+            uint32 vkCode = _WParam;
+            bool wasDown = ((_LParam & (1 << 30)) != 0);
+            bool isDown = ((_LParam & (1 << 31)) == 0);
+            if (wasDown != isDown)
+            {
+                if (vkCode == 'W')
+                {
+                }
+                else if (vkCode == 'A')
+                {
+                }
+                else if (vkCode == 'S')
+                {
+                }
+                else if (vkCode == 'D')
+                {
+                }
+                else if (vkCode == 'Q')
+                {
+                }
+                else if (vkCode == 'E')
+                {
+                }
+                else if (vkCode == VK_UP)
+                {
+                }
+                else if (vkCode == VK_LEFT)
+                {
+                }
+                else if (vkCode == VK_DOWN)
+                {
+                }
+                else if (vkCode == VK_RIGHT)
+                {
+                }
+                else if (vkCode == VK_ESCAPE)
+                {
+                    OutputDebugStringA("ESCAPE: ");
+                    if (isDown)
+                    {
+                        OutputDebugStringA("IsDown ");
+                    }
+                    if (wasDown)
+                    {
+                        OutputDebugStringA("WasDown");
+                    }
+                    OutputDebugStringA("\n");
+                }
+                else if (vkCode == VK_SPACE)
+                {
+                }
+            }
         } break;
         
         case WM_ACTIVATEAPP:
@@ -150,7 +246,7 @@ LRESULT CALLBACK MainWindowCallback(_In_ HWND   _HWnd,
                 // 아래 두 코드가 없어도 제대로 실행된다. 매 프레임 그리기 때문에
                 // 그러나 BeginPaint(), EndPaint()까지 모든 라인을 실행하지 않으면 화면에 그려지는것이 아무것도 없고 먹통이 되어버린다 왜일까??
                 win32_window_dimension dimension = Win32GetWindowDimension(_HWnd);
-                Win32DisplayBufferInWindow(deviceContext, dimension.Width, dimension.Height, &g_backbuffer);
+                Win32DisplayBufferInWindow(&GlobalBackbuffer, deviceContext, dimension.Width, dimension.Height);
             }
             EndPaint(_HWnd, &paint);
         } break;
@@ -170,9 +266,11 @@ int CALLBACK WinMain(_In_ HINSTANCE _HInstance,
                      _In_ LPSTR     _LpCmdLine,
                      _In_ int       _NCmdShow)
 {
+    Win32LoadXInput();
+
     WNDCLASS windowClass = {};
 
-    Win32ResizeDIBSection(&g_backbuffer, 1280, 720);
+    Win32ResizeDIBSection(&GlobalBackbuffer, 1280, 720);
 	
 	windowClass.style = CS_OWNDC|CS_HREDRAW|CS_VREDRAW;
 	windowClass.lpfnWndProc = MainWindowCallback;
@@ -212,28 +310,63 @@ int CALLBACK WinMain(_In_ HINSTANCE _HInstance,
     int32 blueOffset = 0;
     int32 greenOffset = 0;
     
-    g_running = true;
-    while (g_running)
+    GlobalRunning = true;
+    while (GlobalRunning)
     {
         MSG message;
         while (PeekMessage(&message, 0, 0, 0, PM_REMOVE))
         {
             if (message.message == WM_QUIT)
             {
-                g_running = false;
+                GlobalRunning = false;
             }
 
             TranslateMessage(&message);
             DispatchMessageA(&message);
         }
 
-        RenderWeirdGradient(&g_backbuffer, blueOffset, greenOffset);
+        // TODO(jungyoun.la): Should we poll this more frequently.
+        for (DWORD controllerIndex = 0; controllerIndex < XUSER_MAX_COUNT; controllerIndex++)
+        {
+            XINPUT_STATE controllerState;
+            ZeroMemory(&controllerState, sizeof(XINPUT_STATE));
+            if (XInputGetState(controllerIndex, &controllerState) == ERROR_SUCCESS)
+            {
+                // Controller is connected 
+                XINPUT_GAMEPAD* pad = &controllerState.Gamepad;
+
+                bool up = (pad->wButtons & XINPUT_GAMEPAD_DPAD_UP);
+                bool down = (pad->wButtons & XINPUT_GAMEPAD_DPAD_DOWN);
+                bool left = (pad->wButtons & XINPUT_GAMEPAD_DPAD_LEFT);
+                bool right = (pad->wButtons & XINPUT_GAMEPAD_DPAD_RIGHT);
+                bool start = (pad->wButtons & XINPUT_GAMEPAD_START);
+                bool back = (pad->wButtons & XINPUT_GAMEPAD_BACK);
+                bool leftShoulder = (pad->wButtons & XINPUT_GAMEPAD_LEFT_SHOULDER);
+                bool rightShoulder = (pad->wButtons & XINPUT_GAMEPAD_RIGHT_SHOULDER);
+                bool aButton = (pad->wButtons & XINPUT_GAMEPAD_A);
+                bool bButton = (pad->wButtons & XINPUT_GAMEPAD_B);
+                bool xButton = (pad->wButtons & XINPUT_GAMEPAD_X);
+                bool yButton = (pad->wButtons & XINPUT_GAMEPAD_Y);
+
+                int16 stickX = pad->sThumbLX;
+                int16 stickY = pad->sThumbLY;
+
+                blueOffset += stickX >> 12;
+                greenOffset -= stickY >> 12;                
+            }
+            else
+            {
+                // Controller is not connected 
+            }
+        }
+
+        RenderWeirdGradient(&GlobalBackbuffer, blueOffset, greenOffset);
         
         win32_window_dimension dimension = Win32GetWindowDimension(windowHandle);
-        Win32DisplayBufferInWindow(deviceContext, dimension.Width, dimension.Height, &g_backbuffer);
+        Win32DisplayBufferInWindow(&GlobalBackbuffer, deviceContext, dimension.Width, dimension.Height);
 
-        ++blueOffset;
-        greenOffset += 2;
+        /*++blueOffset;
+        greenOffset += 2;*/
     }
 
 	return 0;

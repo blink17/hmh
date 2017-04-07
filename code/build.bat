0bbcdaf1259efae68b5d54aa0d15c0224a49f4b4
unknown <>
2017-04-07 23:44:37 +0900

    27일차 완료

--- code/build.bat
+++ code/build.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set CommonCompilerFlags=-MT -nologo -Gm- -GR- -EHa- -Od -Oi -WX -W4 -wd4201 -wd4100 -wd4189 -DHANDMADE_INTERNAL=1 -DHANDMADE_SLOW=1 -DHANDMADE_WIN32=1 -FC -Z7
+set CommonCompilerFlags=-MTd -nologo -Gm- -GR- -EHa- -Od -Oi -WX -W4 -wd4201 -wd4100 -wd4189 -wd4505 -DHANDMADE_INTERNAL=1 -DHANDMADE_SLOW=1 -DHANDMADE_WIN32=1 -FC -Z7
 set CommonLinkerFlags= -incremental:no -opt:ref user32.lib gdi32.lib winmm.lib
 
 REM TODO - can we just build noth with one exe?
@@ -12,7 +12,8 @@ REM 32-bit build
 REM cl %CommonCompilerFlags% ..\codewin32_handmade.cpp /link -subsystem:windows,5.1 %CommonLinkerFlags%
 
 REM 64-bit build
-cl %CommonCompilerFlags% ..\code\handmade.cpp -Fmhandmade.map /LD /link /EXPORT:GameGetSoundSamples /EXPORT:GameUpdateAndRender
+del *.pdb > NUL 2> NUL
+cl %CommonCompilerFlags% ..\code\handmade.cpp -Fmhandmade.map -LD /link -incremental:no -opt:ref -PDB:handmade_%random%.pdb -EXPORT:GameGetSoundSamples -EXPORT:GameUpdateAndRender /EXPORT:GameUpdateAndRender
 cl %CommonCompilerFlags% ..\code\win32_handmade.cpp -Fmwin32_handmade.map /link %CommonLinkerFlags%
 popd
 
@@ -27,7 +28,7 @@ goto comment
 -Oi 최적화 > 내장 함수 사용 : 내장 함수를 사용하면 더 빠르지만 더 큰 코드가 생성된다.
 -Gm- 코드 생성 > 최소 다시 빌드 가능 : 변경된 C++ 클래스 정의(헤더(.h) 파일에 저장됨)를 포함하는 C++ 소스 파일을 다시 컴파일해야 하는지 여부를 결정하는 최소 다시 빌드를 사용한다. -> 아니요
 -EHa- 코드 생성 > C++ 예외 처리 가능 : 컴파일러가 사용하는 예외 처리 모델을 지정. SEH 예외 있음.
--MT 코드 생성 > 런타임 라이브러리 : 다중 스레드
+-MT 코드 생성 > 런타임 라이브러리 : 다중 스레드 정적 CRT
 -GR- 언어 > 런타임 형식 정보 사용 : 런타임에 C++ 개체의 형식(런타임 형식 정보)를 검사하는 코드를 추가한다. -> 아니요
 -wd4201 -wd4100 -wd4189 고급 > 특정 경고 사용안함
 -FC 진단 메시지에 전체 경로를 사용
@@ -39,4 +40,6 @@ goto comment
 -subsystem:windows 시스템 > 하위 시스템 : 운영체제에서 .exe 파일을 실행하는 방법을 지시할 수 있다.
 -opt:ref 최적화 > 참조 : 참조되지 않은 함수 및/또는 데이터가 제거
 -incremental:no 일반 > 증분 링크 사용 : 증분 링크를 사용하지 않음
+%date:~-4,4%%date:~-10,2%%date:~-7,2%_%time:~0,2%%time:~3,2%%time:~6,2%
+%(date:~-4,4)%%(date:~-10,2)%%(date:~-7,2)%_%(time:~0,2)%%(time:~3,2)%%(time:~6,2)%
 :comment
--- code/handmade.cpp
+++ code/handmade.cpp
@@ -20,38 +20,78 @@ GameOutputSound(game_state *GameState, game_sound_output_buffer *SoundBuffer, in
          ++SampleIndex)
     {
         // TODO(casey): Draw this out for people
+#if 0
         real32 SineValue = sinf(GameState->tSine);
         int16 SampleValue = (int16)(SineValue * ToneVolume);
+#else
+        int16 SampleValue = 0;
+#endif
         *SampleOut++ = SampleValue;
         *SampleOut++ = SampleValue;
 
+#if 0
         GameState->tSine += 2.0f*Pi32*1.0f/(real32)WavePeriod;
         if (GameState->tSine > 2.0f*Pi32)
         {
             GameState->tSine -= 2.0f*Pi32;
         }
+#endif
     }
 }
 
-internal void
-RenderWeirdGradient(game_offscreen_buffer *Buffer, int BlueOffset, int GreenOffset)
+internal int32
+RoundReal32ToInt32(real32 Real32)
 {
-    // TODO(casey): Let's see what the optimizer does
+    int32 Result = (int32)(Real32 + 0.5f);
+    // TODO(casey): Intrinsic????
+    return(Result);
+}
 
-    uint8 *Row = (uint8 *)Buffer->Memory;
-    for (int Y = 0;
-         Y < Buffer->Height;
+internal void
+DrawRectangle(game_offscreen_buffer *Buffer,
+              real32 RealMinX, real32 RealMinY, real32 RealMaxX, real32 RealMaxY,
+              uint32 Color)
+{
+    // TODO(casey): Floating point color tomorrow!!!!!!
+
+    int32 MinX = RoundReal32ToInt32(RealMinX);
+    int32 MinY = RoundReal32ToInt32(RealMinY);
+    int32 MaxX = RoundReal32ToInt32(RealMaxX);
+    int32 MaxY = RoundReal32ToInt32(RealMaxY);
+
+    if (MinX < 0)
+    {
+        MinX = 0;
+    }
+
+    if (MinY < 0)
+    {
+        MinY = 0;
+    }
+
+    if (MaxX > Buffer->Width)
+    {
+        MaxX = Buffer->Width;
+    }
+
+    if (MaxY > Buffer->Height)
+    {
+        MaxY = Buffer->Height;
+    }
+
+    uint8 *Row = ((uint8 *)Buffer->Memory +
+                  MinX*Buffer->BytesPerPixel +
+                  MinY*Buffer->Pitch);
+    for (int32 Y = MinY;
+         Y < MaxY;
          ++Y)
     {
-        uint32 *Pixel = (uint32 *)Row;
-        for (int X = 0;
-             X < Buffer->Width;
+        uint32* Pixel = (uint32 *)Row;
+        for (int32 X = MinX;
+             X < MaxX;
              ++X)
         {
-            uint8 Blue = (uint8)(X + BlueOffset);
-            uint8 Green = (uint8)(Y + GreenOffset);
-
-            *Pixel++ = ((Green << 8) | Blue);
+            *Pixel++ = Color;
         }
 
         Row += Buffer->Pitch;
@@ -67,19 +107,6 @@ extern "C" GAME_UPDATE_AND_RENDER(GameUpdateAndRender)
 	game_state *GameState = (game_state *)Memory->PermanentStorage;
 	if (!Memory->IsInitialized)
 	{
-		char *Filename = __FILE__;
-
-		debug_read_file_result File = Memory->DEBUGPlatformReadEntireFile(Filename);
-		if (File.Contents)
-		{
-            Memory->DEBUGPlatformWriteEntireFile("test.out", File.ContentsSize, File.Contents);
-            Memory->DEBUGPlatformFreeFileMemory(File.Contents);
-		}
-
-		GameState->ToneHz = 512;
-        GameState->tSine = 0.0f;
-
-		// TODO(casey): This may be more appropriate to do in the platform layer
 		Memory->IsInitialized = true;
 	}
 
@@ -91,50 +118,46 @@ extern "C" GAME_UPDATE_AND_RENDER(GameUpdateAndRender)
         if (Controller->IsAnalog)
         {
             // NOTE(casey): Use analog movement tuning
-            GameState->BlueOffset += (int)(4.0f*Controller->StickAverageX);
-            GameState->ToneHz = 512 + (int)(128.0f*Controller->StickAverageY);
         }
         else
         {
             // NOTE(casey): Use digital movement tuning
-            if (Controller->MoveLeft.EndedDown)
-            {
-                GameState->BlueOffset -= 1;
-            }
-
-            if (Controller->MoveRight.EndedDown)
-            {
-                GameState->BlueOffset += 1;
-            }
-        }
-
-        // Input.AButtonEndedDown;
-        // Input.AButtonHalfTransitionCount;
-        if (Controller->ActionDown.EndedDown)
-        {
-            GameState->GreenOffset += 1;
         }
     }
 
-    RenderWeirdGradient(Buffer, GameState->BlueOffset, GameState->GreenOffset);
+    DrawRectangle(Buffer, 0.0f, 0.0f, (real32)Buffer->Width, (real32)Buffer->Height, 0x00FF00FF);
+    DrawRectangle(Buffer, 10.0f, 10.0f, 40.0f, 40.0f, 0x0000FFFF);
 }
 
 extern "C" GAME_GET_SOUND_SAMPLES(GameGetSoundSamples)
 {
     game_state *GameState = (game_state *)Memory->PermanentStorage;
-    GameOutputSound(GameState, SoundBuffer, GameState->ToneHz);
+    GameOutputSound(GameState, SoundBuffer, 400);
 }
 
-#if HANDMADE_WIN32
-#include "windows.h"
-BOOL WINAPI DLLMain(
-    _In_    HINSTANCE hinstDll,
-    _In_    DWORD fdwReason,
-    _In_    LPVOID lpvReserved
-                    )
+/*
+internal void
+RenderWeirdGradient(game_offscreen_buffer *Buffer, int BlueOffset, int GreenOffset)
 {
-    return(true);
+    // TODO(casey): Let's see what the optimizer does
+
+    uint8 *Row = (uint8 *)Buffer->Memory;    
+    for(int Y = 0;
+        Y < Buffer->Height;
+        ++Y)
+    {
+        uint32 *Pixel = (uint32 *)Row;
+        for(int X = 0;
+            X < Buffer->Width;
+            ++X)
+        {
+            uint8 Blue = (uint8)(X + BlueOffset);
+            uint8 Green = (uint8)(Y + GreenOffset);
+
+            *Pixel++ = ((Green << 16) | Blue);
+        }
+        
+        Row += Buffer->Pitch;
+    }
 }
-
-#endif
-
+*/
